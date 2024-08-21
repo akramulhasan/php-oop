afc31e3b3af28ceb6e80cd92ded5a5fc574982e9
Akramul Hasan <lighthouse8048@gmail.com>
2024-08-21 22:21:48 +0600

    new file added

--- cookie/index.php
+++ cookie/index.php
@@ -11,7 +11,7 @@ setcookie('username', 'ak8048', time() + 3600, '/');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coolie Learning</title>
+    <title>Coolie Learning Page</title>
 </head>
 
 <body>
