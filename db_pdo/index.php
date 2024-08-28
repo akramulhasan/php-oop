6f970199b0d82e6f63905bbfa3c1e60ff9cb326b
Akramul Hasan <lighthouse8048@gmail.com>
2024-08-28 09:28:08 +0600

    fetch data to UI from DB

--- db_pdo/index.php
+++ db_pdo/index.php
@@ -9,11 +9,11 @@ $stmt = $pdo->prepare('SELECT * FROM posts');
 $stmt->execute();
 
 // Fetch Result
-$result = $stmt->fetchAll();
+$posts = $stmt->fetchAll();
 
-echo '<pre>';
-var_dump($result);
-echo '</pre>';
+// echo '<pre>';
+// var_dump($result);
+// echo '</pre>';
 
 ?>
 
@@ -34,22 +34,18 @@ echo '</pre>';
         </div>
     </header>
     <div class="container mx-auto p-4 mt-4">
-        <div class="md my-4">
-            <div class="rounded-lg shadow-md">
-                <div class="p-4">
-                    <h2 class="text-xl font-semibold">Post One</h2>
-                    <p class="text-gray-700 text-lg mt-2">This is post one</p>
+        <?php foreach ($posts as $post) : ?>
+            <div class="md my-4">
+                <div class="rounded-lg shadow-md">
+                    <div class="p-4">
+                        <h2 class="text-xl font-semibold"><?= $post['title']; ?></h2>
+                        <p class="text-gray-700 text-lg mt-2"><?= $post['body']; ?></p>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="md my-4">
-            <div class="rounded-lg shadow-md">
-                <div class="p-4">
-                    <h2 class="text-xl font-semibold">Post Two</h2>
-                    <p class="text-gray-700 text-lg mt-2">This is post two</p>
-                </div>
-            </div>
-        </div>
+
+        <?php endforeach; ?>
+
     </div>
 </body>
 
